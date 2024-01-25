bb9fee835173a8b2013597258afed85a53eb7fac
dysarz11 <justindysarz01@gmail.com>
2024-01-24 20:40:33 -0600

    Make loop

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String [] args) {
+        String inputToCloseLoop;
 
+        inputToCloseLoop = " ";
+
+        Scanner kb = new Scanner(System.in);    // make scanner
+
+        while (!inputToCloseLoop.equals("q")) {             //make loop that terminates with input "q"
+            System.out.println("press q to end program");
+            inputToCloseLoop = kb.nextLine();
+        }
     }
 }
